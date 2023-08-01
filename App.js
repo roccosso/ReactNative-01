3ad2d3e94b415d8ebd26824bb043a72987333214
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-01 09:02:30 -0300

    image de fondo

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+const imageExterno_react = {url: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png'}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esta es la aplicacion de Brock y Foxy</Text>
+      <ImageBackground source={(imageExterno_react)}>
+        <Text>Esta es la aplicacion de Brock y Foxy</Text>
+       
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
