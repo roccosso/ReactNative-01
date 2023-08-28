29a4060639d13f4b60e38042248025ded01223fe
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-28 10:36:35 -0300

    tp navegacion

--- App.js
+++ App.js
@@ -1,25 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack' 
+import { NavegationContainer } from '@react-navigation/native' 
 
-const imageExterno_react = {url: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png'}
+const Stack = createStackNavigator()
 
 export default function App() {
+
+  const imageExterno_react = {url: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png'}
+  const [text, setText] = useState('')
+
   return (
-    <View style={styles.container}>
-      <ImageBackground source={(imageExterno_react)}>
-        <Text>Esta es la aplicacion de Brock y Foxy</Text>
-       
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+    <NavegationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Notifications" component={Notifications} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
+    </NavegationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#38DB96',
+    backgroundColor: '#FF0000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  ContainerArriba: {flex: 5,backgroundColor: 'white',},  
+  ContainerAbajo: {flex: 4, backgroundColor: 'blue',},  
+  Container: {
+    backgroundColor: '#0000FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  touchableOpacity: {
+    marginTop: 10,
+    width: '90%',
+    height: 60,
+    backgroundColor: 'black',
+    borderRadius: 15,
+  
+  },
+  textButton: {
+    color: 'white',
+    width: '100%',
+    textAlign: 'center',
+
+    fontSize: 15,
+  },
+  textPressable: {
+    color: 'black',
+    width: '100%',
+    textAlign: 'center',
+
+    fontSize: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+
   },
 });
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    Pressable,
+    Button
+}   from "react-native";
+import kipling from "https://static.wikia.nocookie.net/disney/images/4/4c/Sra._Kipling.png/revision/latest?cb=20130710173048&path-prefix=es";
+
+export default function Login({navigation
+}) {
+
+    const [password, setPassword] = useState("");
+
+    const HandleLogin = () =>{
+        if (password.toLoweCase() === "Kipling") {
+            navegation.navinate('Home')
+        }else{
+            alert('La contraseña es incorrecta')
+        }
+    }
+
+    return (
+    <>
+        <View style={StyleSheet.container}>
+            <Image souerce={kipling} style={StyleSheet.image} />
+            <View style={{ paddingTop: 50, textAlign: "center"}}>
+                <TextInput placeHolder= "nombre" style={styles.input}></TextInput>
+                <TextInput placeHolder= "contraseña" style={styles.input}></TextInput>
+                <Pressable style={styles.button}>
+                    <Text>continuar</Text>
+                </Pressable>
+            </View>
+
+        </View>
+    </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop: 20,
+        flex: 1,
+        backgroundColor: "#fff", 
+        alignItems: "center",
+    },
+    input:{
+        borderColor: "Black",
+        borderWidth: 2,
+        width: 200,
+        height: "auto",
+        padding: 10,
+        margin: 5,
+    },
+    image: {
+        marginTop: 50,
+        width: 200,
+        height: 300,
+    },
+    button: {
+        marginTop: 20,
+        justifyContent: "center",
+        backgroundColor: "red",
+        width: 40,
+        height: 200,
+        borderRadius: 5,
+    },
+});
