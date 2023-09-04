342b9a76d8d129e8e0771b0c817297bbbd874fbb
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-04 10:06:23 -0300

    boton componente

--- src/screens/Screen3.js
+++ src/screens/Screen3.js
@@ -1,15 +1,23 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import Menu from '../components/Menu';
+import BotonReutilizable from '../components/BotonReutilizable';
 
 export default function Screen3({navigation}) {
+
+  const handleLogout = () => {
+    navigation.navigate('LoginScreen');
+  }
+
   return (
     <View style={[styles.container]}>
       <Text style={[styles.text]}>Screen 3</Text>
+      <BotonReutilizable onPress={handleLogout} titulo='LOGOUT' style={styles.button} />
       <Menu navigation={navigation}/>
     </View>
   )
 }
+
 
 const styles = StyleSheet.create({
     container: {
